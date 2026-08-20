9fc35633b81d9798c71f7e80275f5ee897e6c3ba
Shahtukh <alishahrukh551@gmail.com>
2026-08-20 11:42:02 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const menuButton = document.getElementById("menuButton");
+const mobileMenu = document.getElementById("mobileMenu");
+
+if (menuButton && mobileMenu) {
+    menuButton.addEventListener("click", () => {
+        mobileMenu.classList.toggle("open");
+    });
+}
+
+const semesterButton = document.getElementById("semesterButton");
+const semesterMenu = document.getElementById("semesterMenu");
+const semesterArrow = document.getElementById("semesterArrow");
+
+if (semesterButton && semesterMenu) {
+    semesterButton.addEventListener("click", () => {
+        semesterMenu.classList.toggle("hidden");
+
+        if (semesterArrow) {
+            semesterArrow.classList.toggle("rotate-180");
+        }
+    });
+}
